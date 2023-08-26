d017f158c35d5f785d93c2787daeb2c831c7ecfb
douglasbertelli <73892745+douglasbertelli@users.noreply.github.com>
2023-08-25 22:41:54 -0300

    1. Crição a tela de Tabs Task.

--- app/src/main/java/com/example/taskapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/HomeFragment.kt
@@ -8,10 +8,13 @@ import android.view.ViewGroup
 import com.example.taskapp.R
 import com.example.taskapp.databinding.FragmentDoingBinding
 import com.example.taskapp.databinding.FragmentHomeBinding
+import com.example.taskapp.ui.adapter.ViewPageAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment() {
 	private var _binding: FragmentHomeBinding? = null
 	private val binding get() = _binding!!
+
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
@@ -19,6 +22,27 @@ class HomeFragment : Fragment() {
 	): View {
 		_binding = FragmentHomeBinding.inflate(inflater, container, false)
 		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		initTabs()
+	}
+
+	private fun initTabs() {
+		val pageAdapter = ViewPageAdapter(requireActivity())
+		binding.viewPager.adapter = pageAdapter
+
+		pageAdapter.addFragment(TodoFragment(), R.string.status_task_todo)
+		pageAdapter.addFragment(DoingFragment(), R.string.status_task_doing)
+		pageAdapter.addFragment(DoneFragment(), R.string.status_task_done)
+
+		binding.viewPager.offscreenPageLimit = pageAdapter.itemCount
+
+		TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+			tab.text = getString(pageAdapter.getTitle(position))
+		}.attach()
 	}
 
 	override fun onDestroyView() {
