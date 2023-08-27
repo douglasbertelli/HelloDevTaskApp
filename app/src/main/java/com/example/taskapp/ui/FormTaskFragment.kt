e65d46bd5810689e1e70c3801b4712f8549149b2
douglasbertelli <73892745+douglasbertelli@users.noreply.github.com>
2023-08-26 21:36:26 -0300

    1. Atualização da tela de criação de tarefas. 2. Adicionando o Toolbar nas páginas.

---
+++ app/src/main/java/com/example/taskapp/ui/FormTaskFragment.kt
@@ -0,0 +1,27 @@
+package com.example.taskapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskapp.databinding.FragmentFormTaskBinding
+
+class FormTaskFragment : Fragment() {
+	private var _binding: FragmentFormTaskBinding? = null
+	private val binding get() = _binding!!
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		_binding = FragmentFormTaskBinding.inflate(inflater, container, false)
+		return binding.root
+	}
+
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
+	}
+
+}
