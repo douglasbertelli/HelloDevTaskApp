f4c70ec1d06a94dd15b72c07bc981c59069fa64c
douglasbertelli <73892745+douglasbertelli@users.noreply.github.com>
2023-08-30 20:36:22 -0300

    1. Adicionando o serviço do Firebase ao projeto. 2. Adicionando as configurações do Firebase no Gradle. 3. Adicionando função de criar usuário no Firebase.

--- app/src/main/java/com/example/taskapp/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/auth/RegisterFragment.kt
@@ -6,15 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.example.taskapp.R
 import com.example.taskapp.databinding.FragmentRegisterBinding
 import com.example.taskapp.util.initToolbar
 import com.example.taskapp.util.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterFragment : Fragment() {
 	private var _binding: FragmentRegisterBinding? = null
 	private val binding get() = _binding!!
+
+	private lateinit var auth: FirebaseAuth
+
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
@@ -29,6 +36,8 @@ class RegisterFragment : Fragment() {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 		initToolbar(binding.toolbar)
+
+		auth = Firebase.auth
 
 		initListeners()
 	}
@@ -46,13 +55,30 @@ class RegisterFragment : Fragment() {
 
 		if (email.isNotEmpty()) {
 			if (password.isNotEmpty()) {
-				Toast.makeText(requireContext(), "Tudo certo", Toast.LENGTH_SHORT).show()
+
+				binding.progressBar.isVisible = true
+				registerUser(email, password)
+
 			} else {
 				showBottomSheet(message = getString(R.string.password_empty_register_fragment))
 			}
 		} else {
 			showBottomSheet(message = getString(R.string.email_empty_register_fragment))
 		}
+	}
+
+	private fun registerUser(email: String, password: String) {
+		auth.createUserWithEmailAndPassword(email, password)
+			.addOnCompleteListener { task ->
+				if (task.isSuccessful) {
+					findNavController().navigate(R.id.action_global_homeFragment)
+				} else {
+
+					binding.progressBar.isVisible = false
+
+					Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_SHORT).show()
+				}
+			}
 	}
 
 	override fun onDestroyView() {
