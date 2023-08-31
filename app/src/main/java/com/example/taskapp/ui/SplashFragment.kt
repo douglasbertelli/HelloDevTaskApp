bc1df2ebcd090f22918a4cc298c5fd74a8084234
douglasbertelli <73892745+douglasbertelli@users.noreply.github.com>
2023-08-31 10:25:04 -0300

    1. Implementando o logout utilizando o firebase auth.

--- app/src/main/java/com/example/taskapp/ui/SplashFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/SplashFragment.kt
@@ -10,11 +10,16 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.taskapp.R
 import com.example.taskapp.databinding.FragmentSplashBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashFragment : Fragment() {
 
 	private var _binding: FragmentSplashBinding? = null
 	private val binding get() = _binding!!
+
+	private lateinit var auth: FirebaseAuth
 
 	override fun onCreateView(
 		inflater: LayoutInflater,
@@ -29,13 +34,23 @@ class SplashFragment : Fragment() {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
+		auth = Firebase.auth
+
 		// delay de 3 segundo da tela Splash para tela de Login
 		Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 3000)
 	}
 
 	private fun checkAuth() {
-		//navegando da tela de Splash para Login.
-		findNavController().navigate(R.id.action_splashFragment_to_authetication)
+		val currentUser = auth.currentUser
+
+		if(currentUser != null) {
+			// se o usuário estive logado, será redicionado para a tela home.
+			findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+		} else {
+			// navegando da tela de Splash para Login.
+			findNavController().navigate(R.id.action_splashFragment_to_authetication)
+		}
+
 	}
 
 	override fun onDestroyView() {
